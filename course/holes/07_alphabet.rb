ece3faa9c712a5e6e4ec0442aabf6d18460a0bca
Daniel Fone <daniel@fone.net.nz>
2016-03-18 11:43:13 +1300

    Update alphabet hole

--- course/holes/07_alphabet.rb
+++ course/holes/07_alphabet.rb
@@ -1,3 +1,32 @@
-#!/usr/bin/env ruby -w
+# Print the alphabet. No, not that one.
 
-printf 'abcdefghijklmnopqrstuvwxyz'
+ALPHABET = [
+  "α",
+  "β",
+  "γ",
+  "δ",
+  "ε",
+  "ζ",
+  "η",
+  "θ",
+  "ι",
+  "κ",
+  "λ",
+  "μ",
+  "ν",
+  "ξ",
+  "ο",
+  "π",
+  "ρ",
+  "σ",
+  "τ",
+  "υ",
+  "φ",
+  "χ",
+  "ψ",
+  "ω",
+]
+
+ALPHABET.each do |letter|
+  printf letter
+end
